e41b58d22592c5ce03a271594512e943f968f4d4
mewpawee <pawee@alphainnovationslab.io>
2024-07-09 00:24:40 +0700

    add all things

--- cli.ts
+++ cli.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const dir = path.join(__dirname, "..");
 // var dir = path.join(import.meta.dirname, '..')
 
-exec(`PROJECT_ROOT=${dir} npx hardhat --config hardhat.config.ts compile`, { cwd: dir }, (error, stdout, stderr) => {
+exec(`PROJECT_ROOT=${dir} npx hardhat --config contracts/rep1/hardhat.config.ts compile`, { cwd: dir }, (error, stdout, stderr) => {
   if (error) {
     console.log(`error: ${error.message}`);
     return;
--- dist/cli.mjs
+++ dist/cli.mjs
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = path.dirname(__filename);
 var dir = path.join(__dirname, "..");
-exec(`PROJECT_ROOT=${dir} npx hardhat --config hardhat.config.ts compile`, { cwd: dir }, (error, stdout, stderr) => {
+exec(`PROJECT_ROOT=${dir} npx hardhat --config contracts/rep1/hardhat.config.ts compile`, { cwd: dir }, (error, stdout, stderr) => {
   if (error) {
     console.log(`error: ${error.message}`);
     return;
