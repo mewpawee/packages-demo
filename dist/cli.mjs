57330425a1eaae5e0c26a787a5eac1042d6a55e3
mewpawee <pawee@alphainnovationslab.io>
2024-07-10 13:01:52 +0700

    update files

--- dist/cli.mjs
+++ dist/cli.mjs
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
 // cli.ts
-import { exec } from "child_process";
-import path from "path";
-import { fileURLToPath } from "url";
-var __filename = fileURLToPath(import.meta.url);
-var __dirname = path.dirname(__filename);
-var dir = path.join(__dirname, "..");
-exec(`PROJECT_ROOT=${process.cwd()} npx hardhat --config contracts/rep1/hardhat.config.ts compile`, { cwd: dir }, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+import util from "util";
+import child_process from "child_process";
+var exec = util.promisify(child_process.exec);
+var cwd = process.cwd();
+var main = async () => {
+  const target = process.argv[2];
+  console.log("target", target);
+  switch (target) {
+    case "compile":
+      console.log("compiling rep1...");
+      await run(`PROJECT_ROOT=${cwd} npx hardhat --config contracts/rep1/hardhat.config.ts compile`);
+      console.log("compiling rep2...");
+      await run(`PROJECT_ROOT=${cwd} npx hardhat --config contracts/rep2/hardhat.config.ts compile`);
+      break;
+    case "run":
+      await run(`PROJECT_ROOT=${cwd} npx hardhat --config hardhat.config.ts run deploy.ts --no-compile`);
+    default:
   }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+};
+var run = async (cmd) => {
+  const { stdout, stderr } = await exec(cmd);
+  if (stdout) console.log(stdout);
+  if (stderr) console.log(stderr);
+};
+main();
